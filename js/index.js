1cd49c85502110c73da55f2fbd22088a7e0727c2
ManavellaA <andresraulmanavella@gmail.com>
2022-04-27 21:06:29 -0300

    mejoras responsive

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-if(window.innerHeight > 780){
+if(window.innerWidth <= 779){
+    
     let imgHeight = document.querySelector('.view-img').offsetWidth;
     
     imgHeight = imgHeight * 0.6; 
     
-    document.querySelector('.view-div1').style.height = imgHeight + 'px';
-    document.querySelector('.view-div2').style.height = imgHeight + 'px';
-    document.querySelector('.view-div3').style.height = imgHeight + 'px';
-    document.querySelector('.view-div4').style.height = imgHeight + 'px';
-    document.querySelector('.view-div5').style.height = imgHeight + 'px';
+    document.querySelector(".height-js1").style.height = imgHeight + 'px';
+    document.querySelector(".height-js2").style.height = imgHeight + 'px';
+    document.querySelector(".height-js3").style.height = imgHeight + 'px';
+    document.querySelector(".height-js4").style.height = imgHeight + 'px';
+    document.querySelector(".height-js5").style.height = imgHeight + 'px';
 }
