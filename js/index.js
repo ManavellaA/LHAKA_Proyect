fa5c69fc5ca9e336ba0537090e38c00a1b3bdd3c
ManavellaA <andresraulmanavella@gmail.com>
2022-04-28 21:56:04 -0300

    add

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
-function autosize(muliplier) {
-    let imgHeight = document.querySelector('.view-img').offsetWidth;
+// function autosize(muliplier) {
+//     let imgHeight = document.querySelector('.view-img').offsetWidth;
     
-    imgHeight = imgHeight * muliplier; 
+//     imgHeight = imgHeight * muliplier; 
 
-    for(let i = 1; i <= 5; i++) {
-        document.querySelector(`.height-js${i}`).style.height = imgHeight + 'px';    
-    }
-}
+//     for(let i = 1; i <= 5; i++) {
+//         document.querySelector(`.height-js${i}`).style.height = imgHeight + 'px';    
+//     }
+// }
 
-if(window.innerWidth <= 332){
-    autosize(2.5);
-}else if(window.innerWidth <= 437){
-    autosize(1.8);
- }else if(window.innerWidth <= 472){
-     autosize(1.5);
-  }else if(window.innerWidth <= 557){
-     autosize(1.1);  
-   }else if(window.innerWidth <= 779){
-      autosize(0.85);
-    }
+// function sizeAjust(){
+// if(window.innerWidth <= 332){
+//     autosize(2.5);
+// }else if(window.innerWidth <= 437){
+//     autosize(1.8);
+//  }else if(window.innerWidth <= 472){
+//      autosize(1.5);
+//   }else if(window.innerWidth <= 557){
+//      autosize(1.1);  
+//    }else if(window.innerWidth <= 779){
+//       autosize(0.85);
+//     }
+// }
+// sizeAjust();
