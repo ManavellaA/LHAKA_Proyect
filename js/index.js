a3584ac0a06265e732734078594d7a59ba3b3cee
ManavellaA <andresraulmanavella@gmail.com>
2022-05-12 17:42:58 -0300

    add

--- js/index.js
+++ js/index.js
@@ -11,31 +11,4 @@ $(document).ready(function(){
            $a__nav.removeClass('a__nav__scroll');
         }  
     });
- 
-    $('body').height();
-
 });
-// function autosize(multiplier) {
-//     let imgHeight = document.querySelector('.view-img').offsetWidth;
-    
-//     imgHeight = imgHeight * multiplier; 
-
-//     for(let i = 1; i <= 5; i++) {
-//         document.querySelector(`.height-js${i}`).style.height = imgHeight + 'px';    
-//     }
-// }
-
-// function sizeAjust(){
-// if(window.innerWidth <= 332){
-//     autosize(2.5);
-// }else if(window.innerWidth <= 437){
-//     autosize(1.8);
-//  }else if(window.innerWidth <= 472){
-//      autosize(1.5);
-//   }else if(window.innerWidth <= 557){
-//      autosize(1.1);  
-//    }else if(window.innerWidth <= 779){
-//       autosize(0.85);
-//     }
-// }
-// sizeAjust();
