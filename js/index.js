25c77a07ca224d47c454f60cf40e03c73a9d17ee
ManavellaA <andresraulmanavella@gmail.com>
2022-05-14 23:17:38 -0300

    add cata

--- js/index.js
+++ js/index.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){
     let $nav = $('.navbar');
-    let $a__nav = $('.a__nav');
-    $(window).scroll(function(event){
+    $(window).scroll(function(){
        let scroll = $(this).scrollTop();
        if (scroll > 200){
             $nav.addClass('bg__scroll');
-            $a__nav.addClass('a__nav__scroll');       
         } else {
            $nav.removeClass('bg__scroll');
-           $a__nav.removeClass('a__nav__scroll');
         }  
     });
 });
